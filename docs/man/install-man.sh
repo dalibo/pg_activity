928bde7b371d596345c1d9c6467e032790719439
julmon <julien.tachoires@f-secure.com>
2012-11-23 14:12:28 +0100

    remove README.txt and add man page installation using setup.py

--- docs/man/install-man.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo cp -f pg_activity.1.gz /usr/share/man/man1/.
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
-#from distutils.core import setup
-
 from setuptools import setup
+
 setup(
 	name='pg_activity',
 	version='0.1.0',
@@ -10,9 +9,11 @@ setup(
     url='https://github.com/julmon/pg_activity',
     license='LICENSE.txt',
     description='htop like utility for PostgreSQL activity monitoring.',
-    long_description=open('README.txt').read(),
     install_requires=[
         "psutil >= 0.4.1",
         "psycopg2 >= 2.2.1",
     ],
+	data_files=[
+		('/usr/share/man/man1', ['docs/man/pg_activity.1.gz'])
+	]
 )
