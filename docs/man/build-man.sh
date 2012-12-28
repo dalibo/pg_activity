cbb797d37e1a765c80a75021403abfc1219a268d
julmon <julien.tachoires@f-secure.com>
2012-12-28 15:53:30 +0100

    new release : 0.2.2

--- docs/man/build-man.sh
+++ docs/man/build-man.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pod2man -r "pg_activity 0.2.1" -d "2012-12-28" -c "PostgreSQL server activity monitoring tool" pg_activity.pod > pg_activity.1; gzip -f pg_activity.1;
+pod2man -r "pg_activity 0.2.2" -d "2012-12-28" -c "PostgreSQL server activity monitoring tool" pg_activity.pod > pg_activity.1; gzip -f pg_activity.1;
