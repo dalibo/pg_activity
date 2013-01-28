49f98a5c1e482e6356c6de208658bbd38517614b
Julien Tachoires <julmon@gmail.com>
2013-01-28 17:40:01 +0100

    lock management - 1st step

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup
 
 setup(
 	name = 'pg_activity',
-	version = '0.2.3',
+	version = '0.3.0',
 	author = 'Julien Tachoires',
 	author_email = 'julmon@gmail.com',
 	scripts = ['bin/pg_activity'],
-    url = 'https://github.com/julmon/pg_activity',
-    license = 'LICENSE.txt',
-    description = 'htop like utility for PostgreSQL activity monitoring.',
-    install_requires = [
-        "psutil >= 0.4.1",
-        "psycopg2 >= 2.2.1",
-    ],
+	url = 'https://github.com/julmon/pg_activity',
+	license = 'LICENSE.txt',
+	description = 'htop like utility for PostgreSQL activity monitoring.',
+	install_requires = [
+		"psutil >= 0.4.1",
+		"psycopg2 >= 2.2.1",
+	],
 	data_files = [
 		('/usr/share/man/man1', ['docs/man/pg_activity.1.gz'])
 	]
