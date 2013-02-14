3492c70adfb447986c51dcc4d0a50d467162311e
Julien Tachoires <julmon@gmail.com>
2013-02-14 11:52:28 +0100

    bug fixe in pg_is_local() with old psycopg2 driver and new version : 0.3.2

--- docs/man/build-man.sh
+++ docs/man/build-man.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pod2man -r "pg_activity 0.3.1" -d "2013-02-11" -c "PostgreSQL server activity monitoring tool" pg_activity.pod > pg_activity.1; gzip -f pg_activity.1;
+pod2man -r "pg_activity 0.3.2" -d "2013-02-14" -c "PostgreSQL server activity monitoring tool" pg_activity.pod > pg_activity.1; gzip -f pg_activity.1;
