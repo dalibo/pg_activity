ead3dd887d80b5a97125a1ffd0bec9db048cfc47
Denis Laxalde <denis.laxalde@dalibo.com>
2020-07-17 15:24:25 +0200

    Move pg_connect() into utils module and refactor it a bit
    We'll reuse this function in another module of pgactivity package later
    on, hence we need it in that package instead of in the main script.
    
    Along the way, we simplify the logic using a for loop and we add type
    annotations.

--- pgactivity/utils.py
+++ pgactivity/utils.py
@@ -1,4 +1,11 @@
+import getpass
+import optparse
 import re
+import sys
+from typing import Any, Optional
+
+import psycopg2
+from psycopg2 import errorcodes
 
 
 def clean_str(string: str) -> str:
@@ -16,3 +23,42 @@ def clean_str(string: str) -> str:
     msg = re.sub(r"^\s", r"", msg)
     msg = re.sub(r"\s$", r"", msg)
     return msg
+
+
+def pg_connect(
+    data: Any,
+    options: optparse.Values,
+    password: Optional[str] = None,
+    service: Optional[str] = None,
+    exit_on_failed: bool = True,
+) -> Optional[str]:
+    """Try to connect to postgres using 'data' instance, return the password
+    to be used in case of reconnection.
+    """
+    for nb_try in range(2):
+        try:
+            data.pg_connect(
+                host=options.host,
+                port=options.port,
+                user=options.username,
+                password=password,
+                database=options.dbname,
+                rds_mode=options.rds,
+                service=service,
+            )
+        except psycopg2.OperationalError as err:
+            errmsg = str(err).strip()
+            if nb_try < 1 and (
+                err.pgcode == errorcodes.INVALID_PASSWORD
+                or errmsg.startswith("FATAL:  password authentication failed for user")
+                or errmsg == "fe_sendauth: no password supplied"
+            ):
+                password = getpass.getpass()
+            elif exit_on_failed:
+                msg = str(err).replace("FATAL:", "")
+                sys.exit("pg_activity: FATAL: %s" % clean_str(msg))
+            else:
+                raise Exception("Could not connect to PostgreSQL")
+        else:
+            break
+    return password
