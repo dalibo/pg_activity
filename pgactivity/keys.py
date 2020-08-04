dbbd52cb37d7957cb8f8eb8f5ee8bef895c6aea3
Denis Laxalde <denis.laxalde@dalibo.com>
2020-08-04 17:45:03 +0200

    Use QueryMode type in keys module

--- pgactivity/keys.py
+++ pgactivity/keys.py
@@ -1,4 +1,7 @@
+import curses
 from typing import List, Tuple
+
+from .types import QueryMode
 
 
 EXIT = "q"
@@ -23,8 +26,26 @@ BINDINGS: List[Tuple[str, str]] = [
     ("D", "force refresh database size"),
 ]
 
+
+def _sequence_by_int(v: int) -> Tuple[str, str, int]:
+    """
+    >>> _sequence_by_int(11)
+    ('F11', '11', 275)
+    """
+    assert 1 <= v <= 12, v
+    return f"F{v}", str(v), getattr(curses, f"KEY_F{v}")
+
+
+KEYS_BY_QUERYMODE = {
+    QueryMode.activities: _sequence_by_int(1),
+    QueryMode.waiting: _sequence_by_int(2),
+    QueryMode.blocking: _sequence_by_int(3),
+}
+QUERYMODE_FROM_KEYS = {
+    k: qm for qm, keys in KEYS_BY_QUERYMODE.items() for k in keys[1:]
+}
+
+
 MODES: List[Tuple[str, str]] = [
-    ("F1/1", "running queries"),
-    ("F2/2", "waiting queries"),
-    ("F3/3", "blocking queries"),
+    ("/".join(KEYS_BY_QUERYMODE[qm][:-1]), qm.value) for qm in QueryMode
 ]
