b922fd4e6b62afeb807a15baa23fc43008731d0f
Denis Laxalde <denis.laxalde@dalibo.com>
2020-08-05 16:16:32 +0200

    Define globals for sort keys

--- pgactivity/keys.py
+++ pgactivity/keys.py
@@ -8,19 +8,23 @@ EXIT = "q"
 HELP = "h"
 REFRESH_TIME_INCREASE = "+"
 REFRESH_TIME_DECREASE = "-"
+SORTBY_READ = "r"
+SORTBY_WRITE = "w"
+SORTBY_MEM = "m"
+SORTBY_TIME = "t"
 
 BINDINGS: List[Tuple[str, str]] = [
     ("Up/Down", "scroll process list"),
     ("C", "activate/deactivate colors"),
     ("Space", "pause"),
-    ("r", "sort by READ/s desc. (activities)"),
+    (SORTBY_READ, "sort by READ/s desc. (activities)"),
     ("v", "change display mode"),
-    ("w", "sort by WRITE/s desc. (activities)"),
+    (SORTBY_WRITE, "sort by WRITE/s desc. (activities)"),
     (EXIT, "quit"),
     (REFRESH_TIME_INCREASE, "increase refresh time (max:5s)"),
-    ("m", "sort by MEM% desc. (activities)"),
+    (SORTBY_MEM, "sort by MEM% desc. (activities)"),
     (REFRESH_TIME_DECREASE, "decrease refresh time (min:0.5s)"),
-    ("t", "sort by TIME+ desc. (activities)"),
+    (SORTBY_TIME, "sort by TIME+ desc. (activities)"),
     ("R", "force refresh"),
     ("T", "change duration mode"),
     ("D", "force refresh database size"),
