0301e313c1d0eb87cbc14f7f12a38b5519ad82bd
Denis Laxalde <denis.laxalde@dalibo.com>
2020-10-26 09:05:18 +0100

    Move colors definition to a new colors module

--- pgactivity/views.py
+++ pgactivity/views.py
@@ -42,83 +42,8 @@ from .types import (
     SystemInfo,
     UI,
 )
-from . import utils
+from . import colors, utils
 from .activities import sorted as sorted_processes
-
-LINE_COLORS = {
-    "pid": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "database": {
-        "default": "black_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "appname": {
-        "default": "black_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "user": {
-        "default": "black_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "client": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "cpu": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "mem": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "read": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "write": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "time_red": {"default": "red", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "time_yellow": {
-        "default": "yellow",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "time_green": {
-        "default": "green",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "wait_green": {
-        "default": "green_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "wait_red": {
-        "default": "red_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "state_default": {
-        "default": "normal",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "state_yellow": {
-        "default": "yellow",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "state_green": {
-        "default": "green",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "state_red": {"default": "red", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "query": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "relation": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "type": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "mode_yellow": {
-        "default": "yellow_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-    "mode_red": {
-        "default": "red_bold",
-        "cursor": "cyan_reverse",
-        "yellow": "yellow_bold",
-    },
-}
-
 
 line_counter = functools.partial(itertools.count, step=-1)
 
@@ -436,7 +361,7 @@ def processes_rows(
     # color_type = 'yellow'
 
     def color_for(field: str) -> FormattingString:
-        return getattr(term, LINE_COLORS[field][color_type])
+        return getattr(term, colors.FIELD_BY_MODE[field][color_type])
 
     def text_append(value: str) -> None:
         # We also restore 'normal' style so that the next item does not
