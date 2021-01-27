1c4107f17f74d7319c2daac74f8572afa38743de
Denis Laxalde <denis.laxalde@dalibo.com>
2021-01-27 18:27:04 +0100

    Update man page to mention <connection string> argument

--- docs/man/build-man.sh
+++ docs/man/build-man.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pod2man -r "pg_activity 2.0.0" -d `date +%Y-%m-%d` -c "Command line tool for PostgreSQL server activity monitoring." pg_activity.pod > pg_activity.1;
+pod2man -r "pg_activity 2.0.3" -d `date +%Y-%m-%d` -c "Command line tool for PostgreSQL server activity monitoring." pg_activity.pod > pg_activity.1;
