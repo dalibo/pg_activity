c887cccc5031a09724471b5ae61fbfaf17690aac
Denis Laxalde <denis.laxalde@dalibo.com>
2021-02-01 13:49:59 +0100

    Possibly disable border in boxed widget
    This is in order to make UI tests more independent of the number of
    digits in PIDs.

--- pgactivity/widgets.py
+++ pgactivity/widgets.py
@@ -7,17 +7,24 @@ def boxed(
     term: Terminal,
     content: str,
     *,
+    border: bool = True,
     border_color: str = "white",
     center: bool = False,
     width: Optional[int] = None,
 ) -> str:
-    border_width = term.length(content) + 2
-    border_formatter = getattr(term, border_color)
-    lines = [
-        border_formatter("┌" + "─" * border_width + "┐"),
-        " ".join([border_formatter("│") + term.normal, content, border_formatter("│")]),
-        border_formatter("└" + "─" * border_width + "┘") + term.normal,
-    ]
+    if border:
+        border_width = term.length(content) + 2
+        border_formatter = getattr(term, border_color)
+        lines = [
+            border_formatter("┌" + "─" * border_width + "┐"),
+            " ".join(
+                [border_formatter("│") + term.normal, content, border_formatter("│")]
+            ),
+            border_formatter("└" + "─" * border_width + "┘") + term.normal,
+        ]
+    else:
+        # border is disabled in UI tests.
+        lines = ["", content, ""]
     if center:
         if width is None:
             width = term.width
