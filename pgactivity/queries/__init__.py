6c4a9a856748550b54276a0baac7df8f8db4d991
Denis Laxalde <denis.laxalde@dalibo.com>
2021-02-03 09:34:39 +0100

    Remove path existence check in queries.get()
    Path.open() would raise FileNotFoundError, this is enough.

--- pgactivity/queries/__init__.py
+++ pgactivity/queries/__init__.py
@@ -7,7 +7,5 @@ here = pathlib.Path(__file__).parent
 @functools.lru_cache(maxsize=None)
 def get(name: str) -> str:
     path = here / f"{name}.sql"
-    if not path.exists():
-        raise ValueError(name)
     with path.open() as f:
         return f.read()
