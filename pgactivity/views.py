1861f5ee67d37531c0f00cd27899efec8a1d8d53
Denis Laxalde <denis.laxalde@dalibo.com>
2021-05-12 18:54:48 +0200

    Drop a superfluous type hint
    No longer need with recent blessed.

--- pgactivity/views.py
+++ pgactivity/views.py
@@ -74,7 +74,7 @@ def shorten(term: Terminal, text: str, width: Optional[int] = None) -> str:
     """
     if not text:
         return ""
-    wrapped: List[str] = term.wrap(text, width=width, max_lines=1)
+    wrapped = term.wrap(text, width=width, max_lines=1)
     return wrapped[0] + term.normal
 
 
