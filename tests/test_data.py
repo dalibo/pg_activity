8ab2dc07d017ce15054fce83b6d1ee254fcac426
benoit <benoit.lobreau@dalibo.com>
2021-06-25 15:21:50 +0200

    Add test for issue #149
    This commit adds :
    
    * the ability to create a connexion to a different backend with the
      asynchronous "execute" function
    * a test case for the issue #149

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import time
 
 import pytest
+from psycopg2.errors import WrongObjectType
 
 from pgactivity.data import Data
 
@@ -112,6 +113,49 @@ def test_terminate_backend(postgresql, data):
 
 def test_pg_get_active_connections(data, execute):
     assert data.pg_get_active_connections() == 1
-    execute("select pg_sleep(1)")
+    execute("select pg_sleep(2)")
     time.sleep(1)
     assert data.pg_get_active_connections() == 2
+
+
+def test_encoding(postgresql, data, execute):
+    """Test for issue #149"""
+    postgresql.set_session(autocommit=True)
+    with postgresql.cursor() as cur:
+        # plateform specific locales (,Centos, Ubuntu)
+        for encoding in ["fr_FR.latin1", "fr_FR.88591", "fr_FR.8859-1"]:
+            try:
+                cur.execute(
+                    f"CREATE DATABASE latin1 ENCODING 'latin1' TEMPLATE template0 LC_COLLATE '{encoding}' LC_CTYPE '{encoding}'"
+                )
+            except WrongObjectType:
+                continue
+            else:
+                break
+
+    postgresql.set_session(autocommit=False)
+    execute("CREATE TABLE tbl(s text)", dbname="latin1", commit=True)
+    execute(
+        "INSERT INTO tbl(s) VALUES ('initilialized éléphant')",
+        dbname="latin1",
+        commit=True,
+    )
+    execute("UPDATE tbl SET s = 'blocking éléphant'", dbname="latin1")
+    execute("UPDATE tbl SET s = 'waiting éléphant'", dbname="latin1", commit=True)
+    time.sleep(2)
+    running = data.pg_get_activities()
+    assert "éléphant" in running[0].query
+    (waiting,) = data.pg_get_waiting()
+    assert "waiting éléphant" in waiting.query
+    (blocking,) = data.pg_get_blocking()
+    assert "blocking éléphant" in blocking.query
+
+    # Terminate blocking backend in order to avoid side effects in following tests (e.g.
+    # test_ui.txt).
+    data.pg_terminate_backend(blocking.pid)
+    for __ in range(10):
+        if not data.pg_get_waiting():
+            break
+        time.sleep(1)
+    else:
+        raise AssertionError("could not terminate blocking backend")
