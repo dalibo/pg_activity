b9ec0f4b3b794e31831665863effc83ab745739c
Denis Laxalde <denis.laxalde@dalibo.com>
2021-08-05 11:21:00 +0200

    Update version in man page

--- docs/man/build-man.sh
+++ docs/man/build-man.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-version=$(python ../../setup.py --version)
+version=$(python3 ../../setup.py --version)
 
 pod2man -r "pg_activity ${version}" -d `date +%Y-%m-%d` -c "Command line tool for PostgreSQL server activity monitoring." pg_activity.pod > pg_activity.1;
