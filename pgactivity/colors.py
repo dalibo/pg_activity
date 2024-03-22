ffb63351ffda0648dd155c124a03cbec6378e686
Denis Laxalde <denis.laxalde@dalibo.com>
2024-03-22 13:21:31 +0100

    Reformat the color dict for improved readability

--- pgactivity/colors.py
+++ pgactivity/colors.py
@@ -1,7 +1,11 @@
 from . import utils
 
 FIELD_BY_MODE = {
-    "pid": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
+    "pid": {
+        "default": "cyan",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
     "database": {
         "default": "black_bold",
         "cursor": "cyan_reverse",
@@ -17,12 +21,36 @@ FIELD_BY_MODE = {
         "cursor": "cyan_reverse",
         "yellow": "yellow_bold",
     },
-    "client": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "cpu": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "mem": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "read": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "write": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "time_red": {"default": "red", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
+    "client": {
+        "default": "cyan",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "cpu": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "mem": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "read": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "write": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "time_red": {
+        "default": "red",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
     "time_yellow": {
         "default": "yellow",
         "cursor": "cyan_reverse",
@@ -58,10 +86,26 @@ FIELD_BY_MODE = {
         "cursor": "cyan_reverse",
         "yellow": "yellow_bold",
     },
-    "state_red": {"default": "red", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "query": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "relation": {"default": "cyan", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
-    "type": {"default": "normal", "cursor": "cyan_reverse", "yellow": "yellow_bold"},
+    "state_red": {
+        "default": "red",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "query": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "relation": {
+        "default": "cyan",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
+    "type": {
+        "default": "normal",
+        "cursor": "cyan_reverse",
+        "yellow": "yellow_bold",
+    },
     "mode_yellow": {
         "default": "yellow_bold",
         "cursor": "cyan_reverse",
